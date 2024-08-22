837d9b104feaab3b19d09f94158ec3ace4746aa2
uzmoi <rizzzse@gmail.com>
2024-08-22 21:47:40 +0900

    init

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: "esm",
+  sourcemap: true,
+  dts: true,
+  define: {
+    "import.meta.vitest": "null",
+  },
+});
