08567a4dc24ed74bfdcda7de5419b115afe5ed3b
uzmoi <rizzzse@gmail.com>
2025-02-26 03:19:37 +0900

    IdentをID_StartとID_Continueに対応

--- src/parser/utils.ts
+++ src/parser/utils.ts
@@ -6,14 +6,33 @@ export const isDigit = (char: string) => ("\x2f" < char && char < "\x3a");
 export const isAlphabet = (char: string) =>
   ("\x40" < char && char < "\x5b") || ("\x60" < char && char < "\x7b");
 
+// https://www.unicode.org/reports/tr31/#Default_Identifier_Syntax
+// REVIEW: フラグvにする？stringに添え字でアクセスして文字取ってきてるから意味ない？
+const idStartRe = /\p{ID_Start}/u;
+const idContinueRe = /\p{ID_Continue}/u;
+
+/** `ID_Start` + `\` */
 export const isIdentStart = (char: string) =>
-  isAlphabet(char) || char === "\\" || char === "_";
+  // NOTE: 全ての文字ごとに正規表現で判定するのは重いので、ascii文字はそのまま判定する。
+  // https://util.unicode.org/UnicodeJsps/list-unicodeset.jsp?a=[[:ASCII:]%26[:ID_Start:]]
+  isAlphabet(char) || char === "\\" || (char > "\x7f" && idStartRe.test(char));
 
+/** `ID_Continue` + `\` */
 export const isIdentContinue = (char: string) =>
-  isDigit(char) || isAlphabet(char) || char === "\\" || char === "_";
+  // 上のNOTE:と同じく。
+  // https://util.unicode.org/UnicodeJsps/list-unicodeset.jsp?a=[[:ASCII:]%26[:ID_Continue:]]
+  isDigit(char) ||
+  isAlphabet(char) ||
+  char === "\\" ||
+  char === "_" ||
+  (char > "\x7f" && idContinueRe.test(char));
 
+// REVIEW: Pattern_White_Spaceにする？
+// REVIEW: フラグvにする？
 const wsRe = /\p{White_Space}/u;
 export const isWhitespace = (char: string) =>
+  // 上のNOTE:と同じく。
+  // https://util.unicode.org/UnicodeJsps/list-unicodeset.jsp?a=[[:ASCII:]%26[:White_Space:]]
   ("\x08" < char && char < "\x0e") || // "\t" | "\n" | "\v" | "\f" | "\r"
   char === " " ||
   (char > "\x7f" && wsRe.test(char));
@@ -26,5 +45,6 @@ const escapeCharMap = new Map([
   ["v", "\v"],
 ]);
 
+// REVIEW: uかvフラグ付ける？
 export const unescapeStringContent = (string: string): string =>
   string.replace(/\\(.)/g, (_, mat: string) => escapeCharMap.get(mat) ?? mat);
