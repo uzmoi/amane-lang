de5d95528192906705c264ed4990fec01304b029
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-12-16 23:05:23 +0900

    Add types property in Module

--- src/emitter/wasm/module.test.ts
+++ src/emitter/wasm/module.test.ts
@@ -6,7 +6,7 @@ describe("write_module", () => {
   test("validate empty module", () => {
     const writer = new Writer();
     expect(writer.binary.length).toBe(0);
-    write_module(writer, { funcs: [] });
+    write_module(writer, { types: [], funcs: [] });
 
     expect(WebAssembly.validate(writer.binary)).toBeTruthy();
     expect(writer.binary.length).toBe(14);
@@ -14,14 +14,23 @@ describe("write_module", () => {
 
   test("validate simple module", () => {
     const writer = new Writer();
-    write_module(writer, { funcs: [{ body: null }] });
+    write_module(writer, {
+      types: [{ kind: "func", params: [], return: [] }],
+      funcs: [{ signature: 0, body: null }],
+    });
 
     expect(WebAssembly.validate(writer.binary)).toBeTruthy();
   });
 
   test("validate simple module 2", () => {
     const writer = new Writer();
-    write_module(writer, { funcs: [{ body: null }, { body: null }] });
+    write_module(writer, {
+      types: [{ kind: "func", params: [], return: [] }],
+      funcs: [
+        { signature: 0, body: null },
+        { signature: 0, body: null },
+      ],
+    });
 
     expect(WebAssembly.validate(writer.binary)).toBeTruthy();
   });
--- src/emitter/wasm/module.ts
+++ src/emitter/wasm/module.ts
@@ -18,14 +18,16 @@ const enum SectionId {
   data_count = 12,
 }
 
+export type Type = { kind: "func"; params: []; return: [] };
+
 export interface Func {
-  // params: Ty[];
-  // return: Ty;
+  signature: number;
   body: null;
 }
 
 export interface Module {
-  funcs: Func[];
+  types: readonly Type[];
+  funcs: readonly Func[];
 }
 
 export const write_module = (writer: Writer, module: Module) => {
@@ -34,13 +36,21 @@ export const write_module = (writer: Writer, module: Module) => {
     writer.u8(h);
   }
 
-  {
+  if (module.types.length > 0) {
     writer.u8(SectionId.type);
     const ptr = writer.consume(1); // section size
-    writer.u32leb128(1); // types count
-    writer.u32leb128(0x60); // func type
-    writer.u32leb128(0); // params count
-    writer.u32leb128(0); // results count
+    writer.u32leb128(module.types.length);
+    for (const type of module.types) {
+      writer.u32leb128(0x60); // func type
+      writer.u32leb128(type.params.length);
+      for (const _ of type.params) {
+        // writer.u32leb128(NumberType.i32);
+      }
+      writer.u32leb128(type.return.length);
+      for (const _ of type.return) {
+        // writer.u32leb128(NumberType.i32);
+      }
+    }
     writer.fixupSize(ptr);
   }
 
@@ -48,8 +58,8 @@ export const write_module = (writer: Writer, module: Module) => {
     writer.u8(SectionId.func);
     const ptr = writer.consume(1); // section size
     writer.u32leb128(module.funcs.length); // functions count
-    for (const _ of module.funcs) {
-      writer.u32leb128(0); // function signature index
+    for (const func of module.funcs) {
+      writer.u32leb128(func.signature); // function signature index
     }
     writer.fixupSize(ptr);
   }
