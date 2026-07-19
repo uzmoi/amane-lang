ccdfa92b10a5ebb815f0058e4190f608d237d0e7
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-07-20 03:13:57 +0900

    fix(parser): Add ast export
    コミットし忘れじゃね。

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,5 +1,7 @@
 export * from "./lexer";
 export * from "./location";
+// biome-ignore lint/style/useNamingConvention: 😕
+export type * as ast from "./node";
 export { parse_number_literal } from "./number";
 export * from "./parse";
 export { skip_shebang } from "./utils";
